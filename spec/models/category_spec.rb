5721e9e46347ae6219f5e545744cac73036bfe64
adoniyas7 <adoniyasg7s@gmail.com>
2025-05-02 22:45:33 +0300

    add validation tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  attributes = [
+    { name: [ :presence, { uniqueness: { scope: :category_group_id } } ] },
+    { category_group: [ :belong_to ] },
+    { category_attributes: [ :have_many ] },
+    { entity_categories: [ :have_many ] }
+  ]
+
+  include_examples "model_shared_spec", :category, attributes
 end
