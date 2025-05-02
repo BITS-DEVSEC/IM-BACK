3e8f770a76c0518e5c23f85ee3f1aa2aae545829
adoniyas7 <adoniyasg7s@gmail.com>
2025-05-02 22:45:15 +0300

    add validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
   belongs_to :category_group
   has_many :category_attributes
   has_many :entity_categories
+
+  validates :name, presence: true, uniqueness: { scope: :category_group_id }
 end
