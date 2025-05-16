640ce9fba0195a54c37aef4b07bc81cfd9adf31f
adoniyas7 <adoniyasg7s@gmail.com>
2025-05-16 15:32:28 +0300

    add policy scope to index

--- app/controllers/concerns/common.rb
+++ app/controllers/concerns/common.rb
@@ -1,117 +1,128 @@
-module Common
-  extend ActiveSupport::Concern
-  include Pagination
-  include JsonResponse
+  module Common
+    extend ActiveSupport::Concern
+    include Pagination
+    include JsonResponse
 
-  included do
-    before_action :set_clazz
-    before_action :set_object, only: %i[show update]
-  end
-
-  def index
-    data = nil
-    options = {}
-    if block_given?
-      incoming = yield
-      if incoming.instance_of?(Array)
-        data, options = incoming
-      elsif incoming.instance_of?(Hash)
-        options = incoming
-      else
-        data = incoming
-      end
-    else
-      data = @clazz.all
+    included do
+      before_action :set_clazz
+      before_action :set_object, only: %i[show update]
     end
 
-    total = data.count
-    data = data.then(&paginate) if params[:page]
+    def index
+      data = nil
+      options = {}
+      if block_given?
+        incoming = yield
+        if incoming.instance_of?(Array)
+          data, options = incoming
+        elsif incoming.instance_of?(Hash)
+          options = incoming
+        else
+          data = incoming
+        end
+      else
+        data = @clazz.all
+      end
 
-    render_success(nil, data: data, serializer_options: options)
-  end
+      policy_class_name = "#{@clazz.name}Policy"
 
-  def show
-    data = nil
-    options = {}
-    if block_given?
-      incoming = yield
-      if incoming.instance_of?(Array)
-        data, options = incoming
-      elsif incoming.instance_of?(Hash)
+      # Check if the policy class exists to apply policy scope
+      if Object.const_defined?(policy_class_name)
+        begin
+          data = policy_scope(@clazz)
+        rescue => e
+          Rails.logger.warn("PolicyScope failed for #{policy_class_name}: #{e.message}")
+        end
+      end
+
+      total = data.count
+      data = data.then(&paginate) if params[:page]
+
+      render_success(nil, data: data, serializer_options: options)
+    end
+
+    def show
+      data = nil
+      options = {}
+      if block_given?
+        incoming = yield
+        if incoming.instance_of?(Array)
+          data, options = incoming
+        elsif incoming.instance_of?(Hash)
+          data = @obj
+          options = incoming
+        else
+          data = incoming
+        end
+      else
         data = @obj
-        options = incoming
-      else
-        data = incoming
       end
-    else
-      data = @obj
+
+      render_success(nil, data: data, serializer_options: options)
     end
 
-    render_success(nil, data: data, serializer_options: options)
-  end
-
-  def create
-    obj = nil
-    options = {}
-    if block_given?
-      incoming = yield
-      if incoming.instance_of?(Array)
-        obj, options = incoming
-      elsif incoming.instance_of?(Hash)
+    def create
+      obj = nil
+      options = {}
+      if block_given?
+        incoming = yield
+        if incoming.instance_of?(Array)
+          obj, options = incoming
+        elsif incoming.instance_of?(Hash)
+          obj = @clazz.new(model_params)
+          options = incoming
+        else
+          obj = incoming
+        end
+      else
         obj = @clazz.new(model_params)
-        options = incoming
-      else
-        obj = incoming
       end
-    else
-      obj = @clazz.new(model_params)
+
+      if obj.save
+        render_success(nil, data: obj, serializer_options: options, status: :created)
+      else
+        render_error("errors.validation_failed", errors: obj.errors.full_messages[0], status: :unprocessable_entity)
+      end
+    rescue StandardError => e
+      render_error("errors.standard_error", error: e.message)
     end
 
-    if obj.save
-      render_success(nil, data: obj, serializer_options: options, status: :created)
-    else
-      render_error("errors.validation_failed", errors: obj.errors.full_messages[0], status: :unprocessable_entity)
-    end
-  rescue StandardError => e
-    render_error("errors.standard_error", error: e.message)
-  end
-
-  def update
-    obj = nil
-    options = {}
-    if block_given?
-      incoming = yield
-      if incoming.instance_of?(Array)
-        obj, options = incoming
-      elsif incoming.instance_of?(Hash)
+    def update
+      obj = nil
+      options = {}
+      if block_given?
+        incoming = yield
+        if incoming.instance_of?(Array)
+          obj, options = incoming
+        elsif incoming.instance_of?(Hash)
+          obj = set_object
+          options = incoming
+        else
+          obj = incoming
+        end
+      else
         obj = set_object
-        options = incoming
-      else
-        obj = incoming
       end
-    else
-      obj = set_object
+
+      if obj.update(model_params)
+        render_success(nil, data: obj, serializer_options: options)
+      else
+        render_error("errors.validation_failed", errors: obj.errors.full_messages[0], status: :unprocessable_entity)
+      end
+    rescue StandardError => e
+      render_error("errors.standard_error", error: e.message)
     end
 
-    if obj.update(model_params)
-      render_success(nil, data: obj, serializer_options: options)
-    else
-      render_error("errors.validation_failed", errors: obj.errors.full_messages[0], status: :unprocessable_entity)
+    private
+
+    def set_clazz
+      @clazz = controller_name.classify.constantize
     end
-  rescue StandardError => e
-    render_error("errors.standard_error", error: e.message)
+
+    def set_object
+      @obj = @clazz.find(params[:id])
+    end
+
+    # This method should be overridden by respective child controllers
+    def model_params; end
   end
-
-  private
-
-  def set_clazz
-    @clazz = controller_name.classify.constantize
-  end
-
-  def set_object
-    @obj = @clazz.find(params[:id])
-  end
-
-  # This method should be overridden by respective child controllers
-  def model_params; end
-end
