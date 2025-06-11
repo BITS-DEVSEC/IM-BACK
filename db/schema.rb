63cf4f388ab9f8d39035984c7a437cf2e9a9f007
adoniyas7 <adoniyasg7s@gmail.com>
2025-06-11 03:32:14 +0300

    add temporary_password column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_06_124925) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_10_222752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -339,6 +339,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_06_124925) do
     t.boolean "verified", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "temporary_password", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["fin"], name: "index_users_on_fin", unique: true
     t.index ["phone_number"], name: "index_users_on_phone_number", unique: true
