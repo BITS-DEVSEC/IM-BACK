7a445271604be3a25bbf0b69e7b53c58061fdd97
adoniyas7 <adoniyasg7s@gmail.com>
2025-06-11 03:05:35 +0300

    add logo attachment validation to insurer model

--- app/models/insurer.rb
+++ app/models/insurer.rb
@@ -1,8 +1,22 @@
 class Insurer < ApplicationRecord
   belongs_to :user
   has_many :insurance_products
+  has_one_attached :logo
 
   validates :name, presence: true, uniqueness: true
   validates :contact_email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :contact_phone, presence: true
+
+  private
+  def validate_logo
+    return unless logo.attached?
+
+    unless logo.content_type.in?(%w[image/jpeg image/png])
+      errors.add(:logo, "must be a JPEG or PNG")
+    end
+
+    if logo.byte_size > 5.megabytes
+      errors.add(:logo, "must be less than 5MB")
+    end
+  end
 end
--- spec/factories/insurers.rb
+++ spec/factories/insurers.rb
@@ -6,6 +6,12 @@ FactoryBot.define do
     contact_email { Faker::Internet.email }
     contact_phone { Faker::PhoneNumber.phone_number }
 
+    after(:build) do |insurer|
+      image_path = Rails.root.join('spec/fixtures/files/sample_image.jpg')
+      file = Rack::Test::UploadedFile.new(image_path, 'image/jpeg')
+      insurer.logo.attach(file)
+    end
+
     trait :with_products do
       after(:create) do |insurer|
         create_list(:insurance_product, 3, insurer: insurer)
