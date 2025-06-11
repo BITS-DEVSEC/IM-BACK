bf9b98aed647105543c0acee567303d80152158b
adoniyas7 <adoniyasg7s@gmail.com>
2025-06-11 03:19:02 +0300

    add logo validations

--- spec/models/insurer_spec.rb
+++ spec/models/insurer_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Insurer, type: :model do
+  logo_validations = [
+    { content_type: {
+        allowed: [ 'image/jpeg', 'image/png' ],
+        message: 'must be a JPEG or PNG'
+      }
+    },
+    { size: {
+        less_than: 5.megabytes,
+        message: 'must be less than 5MB'
+      }
+    }
+  ]
+
   attributes = [
     { user: [ :belong_to ] },
     { insurance_products: [ :have_many ] },
     { name: [ :presence, :uniqueness ] },
     { contact_email: [ :presence ] },
-    { contact_phone: [ :presence ] }
+    { contact_phone: [ :presence ] },
+    { logo: logo_validations }
   ]
 
   include_examples "model_shared_spec", :insurer, attributes
